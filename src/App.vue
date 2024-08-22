791680032e410cc3bca756881dc37318f14f70b6
Николай <novikovn383@gmail.com>
2024-08-22 14:43:41 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,171 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <section class="main">
+      <div class="container">
+        <h1 class="main__title">Обменник валют</h1>
+        <p class="main__descript">Проверьте цену валюты в реальном времени</p>
+        <div class="main__block">
+          <div class="main__item">
+            <h2 class="main__item-title">Ковертировать из: </h2>
+            <div class="main__item-block">
+              <select 
+              class="main__item-select"
+              v-model="fromCurr"
+              >
+              <option disabled hidden >RUB</option>
+              <option 
+                v-for="CurrItem in this.CURRLIST"
+                :key="CurrItem"
+                >{{CurrItem}}</option>
+              </select>
+              <input 
+              class="main__item-value"
+              v-model="fromValue"
+              type="number"
+              min="0"
+              />
+            </div>
+          </div>
+          <div class="main__item">
+            <h2 class="main__item-title">Конвертировать в: </h2>
+            <div class="main__item-block">
+              <select 
+              class="main__item-select" 
+              v-model="toCurr"
+              >
+                <option disabled hidden>RUB</option>
+                <option 
+                v-for="CurrItem in this.CURRLIST"
+                :key="CurrItem"
+                >{{CurrItem}}</option>
+              </select>
+              <input 
+              class="main__item-value"
+              v-model="ActualCurr"
+              readonly
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapActions, mapGetters} from 'vuex'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      fromValue: 0,
+      fromCurr: '',
+      toCurr: ''
+      
+    }
+  },
+  computed: {
+    ...mapGetters(['CURRLIST', 'CURRVALUE']),
+    ActualCurr(){
+      return this.CURRVALUE(this.toCurr) && this.fromValue > 0 ?  this.CURRVALUE(this.toCurr) * this.fromValue : 0
+    }
+  },
+  methods: {
+    ...mapActions([
+      'GET_LIST_OF_CURRENCIES_FROM_DB',
+      'GET_CURR_PRICE'
+    ])
+  },
+  watch: {
+    fromValue(){
+      let FromValueConditions = this.fromValue <= 0 
+      let FromCurrConditions = this.fromCurr.length == 0
+      let ToCurrConditions = this.toCurr.length == 0
+      if (FromValueConditions || FromCurrConditions || ToCurrConditions) return 
+      this.GET_CURR_PRICE(this.fromCurr)
+    }
+  },
+
+  mounted(){
+    this.GET_LIST_OF_CURRENCIES_FROM_DB()
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+$white: #fff
+$blue: #1F2261
+$gray: #808080
+$black: #000000
+*
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+  font-family: "Roboto", sans-serif
+input 
+  border: 0 none 
+  outline: 0 none
+body 
+  background: #F6F6F6
+.main
+  width: 100%
+  height: 100dvh
+  display: flex
+  align-items: center
+  justify-content: center
+  .container
+    max-width: 550px
+    width: 100%
+    padding: 0px 20px
+  &__title 
+    color: $blue
+    text-align: center
+    font-size: 25px
+    font-weight: 700
+    margin-bottom: 10px
+  &__descript
+    text-align: center
+    font-size: 16px
+  &__block 
+    background: $white
+    width: 100%
+    padding: 20px
+    border-radius: 10px
+    margin: 0 auto
+    margin-top: 40px
+    display: flex
+    flex-direction: column
+    gap: 20px
+  &__item
+    &-title 
+      font-size: 15px
+      color: $gray
+      font-weight: 400
+    &-block 
+      display: flex
+      align-items: center
+      justify-content: space-between
+      margin-top: 10px
+    &-select 
+      border: 0 none 
+      outline: 0 none
+      max-width: 123px
+      width: 100%
+      font-size: 20px
+      font-weight: 500
+      color: $blue
+      text-transform: uppercase
+      cursor: pointer
+      border: 1px solid #EFEFEF
+      border-radius: 5px
+      padding: 5px 10px 
+    &-value 
+      padding: 10px 
+      border-radius: 10px
+      background: #EFEFEF
+      color: $black
+      text-align: right
+      font-size: 20px
+      font-weight: 600
+      max-width: 141px
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,61 @@
 import { createStore } from 'vuex'
-
+import axios from 'axios';
+/* eslint-disable */
 export default createStore({
   state: {
+    CurrList: [],
+    CurrValue: {}
   },
   getters: {
+    CURRLIST(state){
+      return state.CurrList
+    },
+    CURRVALUE: (state) => (toCurr) => {
+      return state.CurrValue[toCurr]
+    }
   },
   mutations: {
+    SET_CURR_TO_STATE(state, data){
+      state.CurrList = data
+    },
+    SET_CURRENT_CURR_TO_STATE(state, data){
+      state.CurrValue = data
+    }
   },
   actions: {
+    async GET_LIST_OF_CURRENCIES_FROM_DB({commit}){
+      try {
+        await axios.get('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json')
+        .then((res) => {
+          commit('SET_CURR_TO_STATE', Object.keys(res.data))
+        })
+        .catch((err) => {
+          console.log('Не удалось получить валюты: ' + err)
+          return err
+          
+        })
+      } catch(err){
+        console.log('Не удалось получить валюты: ' + err)
+        return err
+      }
+    },
+    async GET_CURR_PRICE({commit}, curr){
+      let currLink = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${curr}.json`
+      try {
+        await axios.get(currLink)
+        .then((res) => {
+          commit('SET_CURRENT_CURR_TO_STATE', res.data[curr])
+        })
+        .catch((err) => {
+          console.log('Не удалось получить данные по валюте: ' + err)
+          return err
+        })
+      } catch(err){
+        console.log('Не удалось получить данные по валюте: ' + err)
+        return err
+        
+      }
+    }
   },
   modules: {
   }
